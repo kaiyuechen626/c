0ccdac49b7ae6a1a25c54a9ad40849c5c781787b
Kaixuan Chen <837470933@shu.edu.cn>
2025-07-15 21:53:50 +0800

    Add files via upload

---
+++ ckx/At_Trans_seq2seq.py
@@ -0,0 +1,12 @@
+from AttentionDecoder import *
+from AttentionEncoder import *
+from Seq2seq import Seq2seq
+class AttentionSeq2seq(Seq2seq):
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        args = vocab_size, wordvec_size, hidden_size
+        self.encoder = AttentionEncoder(*args)
+        self.decoder = AttentionDecoder(*args)
+        self.softmax = TimeSoftmaxWithLoss()
+
+        self.params = self.encoder.params + self.decoder.params
+        self.grads = self.encoder.grads + self.decoder.grads
